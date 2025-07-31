daf1ffbdb2e3d8db5aec95bb474ef8eb785810d8
Manuel M. <mamueluth@gmail.com>
2025-07-31 22:26:31 +0200

    Remove not needed stuff, template print function

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,4 @@
-/****************************************************************
- * Example1_Basics.ino
- * ICM 20948 Arduino Library Demo
- * Use the default configuration to stream 9-axis IMU data
- * Owen Lyke @ SparkFun Electronics
- * Original Creation Date: April 17 2019
- *
- * Please see License.md for the license information.
- *
- * Distributed as-is; no warranty is given.
- ***************************************************************/
-#include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
+#include "ICM_20948.h" 
 
 //#define USE_SPI       // Uncomment this to use SPI
 
@@ -28,6 +17,11 @@ ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
 #else
 ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
 #endif
+
+// forward declaration of the printScaledAGMT function
+template <typename CommunicationProtocol> void printScaledAGMT(CommunicationProtocol *sensor);
+const uint8_t float_allignment = 5;
+const uint8_t float_precision = 4;
 
 void setup()
 {
@@ -61,7 +55,7 @@ void setup()
     if (myICM.status != ICM_20948_Stat_Ok)
     {
       SERIAL_PORT.println("Trying again...");
-      delay(500);
+      delay(1000);
     }
     else
     {
@@ -69,8 +63,6 @@ void setup()
     }
   }
 }
-
-void printScaledAGMT(ICM_20948_I2C *sensor);
 
 void loop()
 {
@@ -80,86 +72,12 @@ void loop()
     myICM.getAGMT();         // The values are only updated when you call 'getAGMT'
                              //    printRawAGMT( myICM.agmt );     // Uncomment this to see the raw values, taken directly from the agmt structure
     printScaledAGMT(&myICM); // This function takes into account the scale settings from when the measurement was made to calculate the values with units
-    delay(30);
   }
   else
   {
     SERIAL_PORT.println("Waiting for data");
-    delay(500);
+    delay(1);
   }
-}
-
-// Below here are some helper functions to print the data nicely!
-
-void printPaddedInt16b(int16_t val)
-{
-  if (val > 0)
-  {
-    SERIAL_PORT.print(" ");
-    if (val < 10000)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (val < 1000)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (val < 100)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (val < 10)
-    {
-      SERIAL_PORT.print("0");
-    }
-  }
-  else
-  {
-    SERIAL_PORT.print("-");
-    if (abs(val) < 10000)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (abs(val) < 1000)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (abs(val) < 100)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (abs(val) < 10)
-    {
-      SERIAL_PORT.print("0");
-    }
-  }
-  SERIAL_PORT.print(abs(val));
-}
-
-void printRawAGMT(ICM_20948_AGMT_t agmt)
-{
-  SERIAL_PORT.print("RAW. Acc [ ");
-  printPaddedInt16b(agmt.acc.axes.x);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.acc.axes.y);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.acc.axes.z);
-  SERIAL_PORT.print(" ], Gyr [ ");
-  printPaddedInt16b(agmt.gyr.axes.x);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.gyr.axes.y);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.gyr.axes.z);
-  SERIAL_PORT.print(" ], Mag [ ");
-  printPaddedInt16b(agmt.mag.axes.x);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.mag.axes.y);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.mag.axes.z);
-  SERIAL_PORT.print(" ], Tmp [ ");
-  printPaddedInt16b(agmt.tmp.val);
-  SERIAL_PORT.print(" ]");
-  SERIAL_PORT.println();
 }
 
 void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
@@ -203,33 +121,28 @@ void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
   }
 }
 
-#ifdef USE_SPI
-  void printScaledAGMT(ICM_20948_SPI *sensor)
-  {
-#else
-  void printScaledAGMT(ICM_20948_I2C *sensor)
-  {
-#endif
-    SERIAL_PORT.print("Scaled. Acc (mg) [ ");
-    printFormattedFloat(sensor->accX(), 5, 2);
-    SERIAL_PORT.print(", ");
-    printFormattedFloat(sensor->accY(), 5, 2);
-    SERIAL_PORT.print(", ");
-    printFormattedFloat(sensor->accZ(), 5, 2);
-    SERIAL_PORT.print(" ], Gyr (DPS) [ ");
-    printFormattedFloat(sensor->gyrX(), 5, 2);
-    SERIAL_PORT.print(", ");
-    printFormattedFloat(sensor->gyrY(), 5, 2);
-    SERIAL_PORT.print(", ");
-    printFormattedFloat(sensor->gyrZ(), 5, 2);
-    SERIAL_PORT.print(" ], Mag (uT) [ ");
-    printFormattedFloat(sensor->magX(), 5, 2);
-    SERIAL_PORT.print(", ");
-    printFormattedFloat(sensor->magY(), 5, 2);
-    SERIAL_PORT.print(", ");
-    printFormattedFloat(sensor->magZ(), 5, 2);
-    SERIAL_PORT.print(" ], Tmp (C) [ ");
-    printFormattedFloat(sensor->temp(), 5, 2);
-    SERIAL_PORT.print(" ]");
-    SERIAL_PORT.println();
-  }
+template <typename CommunicationProtocol> void printScaledAGMT(CommunicationProtocol *sensor)
+{
+  SERIAL_PORT.print("Scaled. Acc (mg) [ ");
+  printFormattedFloat(sensor->accX(), float_allignment, float_precision);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accY(), float_allignment, float_precision);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accZ(), float_allignment, float_precision);
+  SERIAL_PORT.print(" ], Gyr (DPS) [ ");
+  printFormattedFloat(sensor->gyrX(), float_allignment, float_precision);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->gyrY(), float_allignment, float_precision);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->gyrZ(), float_allignment, float_precision);
+  SERIAL_PORT.print(" ], Mag (uT) [ ");
+  printFormattedFloat(sensor->magX(), float_allignment, float_precision);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->magY(), float_allignment, float_precision);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->magZ(), float_allignment, float_precision);
+  SERIAL_PORT.print(" ], Tmp (C) [ ");
+  printFormattedFloat(sensor->temp(), float_allignment, float_precision);
+  SERIAL_PORT.print(" ]");
+  SERIAL_PORT.println();
+}
